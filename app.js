5940e759f7c5a0e0cbd04a0b6d0a8358850e38d6
ram720800 <code72hub@gmail.com>
2024-10-18 22:00:02 +0530

    initial commit to my portfolio

---
+++ app.js
@@ -0,0 +1,17 @@
+function updateTime() {
+    const clockElement = document.querySelector('.clock');
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    const timeString = `${hours} : ${minutes} : ${seconds}`;
+    clockElement.textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
